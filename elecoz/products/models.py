65e8ab9a743d641528ee5ffe7117c0f3c297defe
Rohithburri <burrirohith3@gmail.com>
2026-05-28 14:38:20 +0530

    changes in products

--- elecoz/products/models.py
+++ elecoz/products/models.py
@@ -1,15 +1,4 @@
 from django.db import models
-
-class Category(models.Model):
-    # name = models.CharField(max_length=100)
-    name = models.CharField(max_length=100, unique=True)
-    image = models.ImageField(upload_to="categories/", null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-
-from django.db import models
 from django.utils.text import slugify
 
 
@@ -44,8 +33,6 @@ class SubCategory(models.Model):
         return f"{self.category.name} - {self.name}"
 
 
-from django.utils.text import slugify
-
 class Brand(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True, blank=True)
@@ -61,7 +48,6 @@ class Brand(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    # code = models.CharField(max_length=200, blank=True)
     code = models.CharField(max_length=200, unique=True)
 
     price = models.FloatField()
@@ -73,35 +59,42 @@ class Product(models.Model):
     breaking_capacity = models.CharField(max_length=50, blank=True)
     setting_type = models.CharField(max_length=50, blank=True)
 
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
-    image2 = models.ImageField(upload_to='products/', null=True, blank=True)
-    pdf = models.FileField(upload_to='pdfs/', blank=True, null=True)
+    image = models.ImageField(upload_to="products/", blank=True, null=True)
+    image2 = models.ImageField(upload_to="products/", null=True, blank=True)
+    pdf = models.FileField(upload_to="pdfs/", blank=True, null=True)
 
     auxiliary_contact = models.CharField(max_length=100, blank=True, null=True)
-
     coil_voltage = models.CharField(max_length=100, blank=True, null=True)
-
     duty = models.CharField(max_length=100, blank=True, null=True)
 
     stock_status = models.CharField(max_length=100, blank=True, null=True)
-
     dispatch_date = models.CharField(max_length=100, blank=True, null=True)
-
     warehouse = models.CharField(max_length=200, blank=True, null=True)
 
     quantity = models.IntegerField(default=0)
 
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-    # ✅ ADD THIS
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True)
+    subcategory = models.ForeignKey(
+        SubCategory,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="products"
+    )
+
+    brand = models.ForeignKey(
+        Brand,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+
     def save(self, *args, **kwargs):
         if self.quantity <= 0:
             self.stock_status = "Out Of Stock"
-
         elif self.quantity <= 2:
             self.stock_status = "Limited Stock"
-
         else:
             self.stock_status = "In Stock"
 
@@ -109,5 +102,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-
-
